959713e3cd7a440aae74f6baba6161df09fcd499
rahulsrajput <rahulsrajput29@gmail.com>
2024-03-17 13:04:14 +0530

    .

--- photoshare/urls.py
+++ photoshare/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,5 @@ urlpatterns = [
     path('logoutUser/', views.logoutUser, name='logout'),
     path('add/', views.addPhoto, name='add'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
